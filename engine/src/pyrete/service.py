39f88d9af9e6d21d77b85df7937bac948925772c
Amit Chatterjee <amit.chatterjee@quik-j.com>
2024-12-04 15:34:26 -0500

    Changed the name, manual, to repository

--- engine/src/pyrete/service.py
+++ engine/src/pyrete/service.py
@@ -2,9 +2,9 @@ import uuid
 from session import Session
 from repository import Repository
 
-def execute(manual, facts, global_ctx={}):
+def execute(repository, facts, global_ctx={}):
     resulting_facts = facts
-    for ruleset in manual.rulesets:
-        session = Session(ruleset, resulting_facts, f"{manual.id}:{ruleset.id}", global_ctx)
+    for ruleset in repository.rulesets:
+        session = Session(ruleset, resulting_facts, f"{repository.id}:{ruleset.id}", global_ctx)
         resulting_facts = session.execute()
     return resulting_facts
