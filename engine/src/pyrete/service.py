ddbeb1d6d976328c3ffbb2a05d0983386df7e3eb
Amit Chatterjee <amit.chatterjee@quik-j.com>
2024-12-18 10:54:25 -0500

    Added more log standardization

--- engine/src/pyrete/service.py
+++ engine/src/pyrete/service.py
@@ -10,14 +10,14 @@ def __find_switch(facts):
     return None
         
 def execute(repository, facts, global_ctx={}, start_from=None):
-    service_id = int(round(time() * 1000))
+    service_id = f"{repository.id}:{int(round(time() * 1000))}"
     logging.debug(f"Executing service: {service_id}")
     resulting_facts = facts
     for ruleset in repository.rulesets:
         if start_from and ruleset.id != start_from:
             continue
         logging.debug(f"Creating session with service Id: {service_id}, ruleset: {ruleset}, facts:{facts}")
-        session = Session(ruleset, resulting_facts, f"{repository.id}:{ruleset.id}:{service_id}", global_ctx)
+        session = Session(ruleset, resulting_facts, f":{service_id}:{ruleset.id}", global_ctx)
         resulting_facts = session.execute()
         logging.debug(f"Executed session: {session}")
         if switch_to := __find_switch(resulting_facts):
